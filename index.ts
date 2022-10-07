09099dde4289e650bc8361f6a2904715a53c9756
unknown <vaughnjbauer@gmail.com>
2022-10-06 21:11:19 -0500

    Add cors headers

--- index.ts
+++ index.ts
@@ -38,6 +38,19 @@ const albumRouter = new AlbumRouter(albumDatabase, reviewDatabase);
 const reviewRouter = new ReviewRouter(reviewDatabase);
 const ratingRouter = new RatingRouter(ratingCalculator);
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+
+  if(req.method === 'OPTIONS') {
+    res.sendStatus(200);
+    return;
+  }
+
+  next();
+});
+
 app.use(bodyParser.json())
 
 app.get('/rating/get', ratingRouter.get);
